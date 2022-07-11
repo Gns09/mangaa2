66939dd85e6fc2ba5b2e81c0e340345f38340885
Gns <97271133+Gns09@users.noreply.github.com>
2022-07-11 17:01:14 +0530

    Update progress.py

--- plugins/progress.py
+++ plugins/progress.py
@@ -10,8 +10,5 @@ def download(bot, update):
     if update.from_user.id != Config.OWNER_ID:
         return
     url = update.text
-    if check_url(url):
-        pass
-    else:
-        return
+
     update.reply_text(Translation.UPLOAD, quote=True, reply_markup=InlineKeyboardMarkup(Translation.upload_buttons))
